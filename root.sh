fe2f404b7bb5c922c2abb2ccbd83d437fef89b43
KaliRootSuperUser <kalirootsuperuser@gmail.com>
2025-11-28 21:47:50 +0100

    Update root.sh

--- root.sh
+++ root.sh
@@ -69,7 +69,7 @@ fi
 cd /tmp
 echo '[+] Downloading kernel...'
 echo "${KSU_VER}/kernel-ARCVM-${ARCH}-${KERNEL_VER}.zip"
-curl -L -'#' "https://github.com/KaliRootSuperUser/CHROME-OS-ARCVM-ROOTER/releases/download/KRSU/ARCVM-BZIMAGE-SUKISU-ULTRA-SUSFS-KERNEL.zip" -o ksu.zip
+curl -L -'#' "https://github.com/KaliRootSuperUser/CHROME-OS-ARCVM-ROOTER/releases/download/v2.0/kernel-ARCVM-x86_64-5.10.239.zip" -o ksu.zip
 
 echo '[+] Decompressing kernel...'
 mkdir -p ksu
